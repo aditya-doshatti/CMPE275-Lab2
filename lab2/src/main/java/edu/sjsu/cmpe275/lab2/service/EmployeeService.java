78fb3f74c943e91e3206dcafea6c2cc0b9a4a95e
Anand Kumar <anandk174@gmail.com>
2019-04-14 17:58:11 -0700

    Documentation

--- lab2/src/main/java/edu/sjsu/cmpe275/lab2/service/EmployeeService.java
+++ lab2/src/main/java/edu/sjsu/cmpe275/lab2/service/EmployeeService.java
@@ -9,18 +9,32 @@ import org.springframework.stereotype.Service;
 import edu.sjsu.cmpe275.lab2.model.Employee;
 import edu.sjsu.cmpe275.lab2.repository.EmployeeRepository;
 
+/**
+ * Service class for employee repository functions
+ * @return
+ */
 @Service
 public class EmployeeService {
 	
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	
+	/**
+	 * fetches all employees
+	 * @return employee list
+	 */
 	public List<Employee> getAllEmployees() {
 		List<Employee> empList = new ArrayList<Employee>();
 		employeeRepository.findAll().forEach(empList::add);
 		return empList;
 	}
-	
+
+	/**
+	 * fetches a particular employee
+	 * @param id accepts employee ID
+	 * @return employee information
+	 */
 	public Employee getEmployee(Long id) {
 		if(id != null)
 			return employeeRepository.findOne(id);
@@ -28,18 +42,35 @@ public class EmployeeService {
 			return null;
 	}
 
+	/**
+	 * adds employee to employee repository
+	 * @param emp employee object
+	 */
 	public void addEmployee(Employee emp) {
 		employeeRepository.save(emp);
 	}
 
+	/**
+	 * updates employee information
+	 * @param emp employee object
+	 */
 	public void updateEmployee(Employee emp) {
 		employeeRepository.save(emp);
 	}
 
+	/**
+	 * deletes employee from repository
+	 * @param id employee ID
+	 */
 	public void deleteEmployee(long id) {
 		employeeRepository.delete(id);
 	}
 
+	/**
+	 * adds collaboration between employees
+	 * @param emp1 first employee object
+	 * @param emp2 second employee object
+	 */
 	public void addCollaboration(Employee emp1, Employee emp2) {
 		employeeRepository.save(emp1);
 		employeeRepository.save(emp2);
--- lab2/src/main/java/edu/sjsu/cmpe275/lab2/service/EmployerService.java
+++ lab2/src/main/java/edu/sjsu/cmpe275/lab2/service/EmployerService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Service;
 import edu.sjsu.cmpe275.lab2.model.Employer;
 import edu.sjsu.cmpe275.lab2.repository.EmployerRepository;
 
+/**
+ * Employer Service 
+ */
 @Service
 public class EmployerService {
 	
@@ -16,24 +19,46 @@ public class EmployerService {
 	private EmployerRepository employerRepository;
 
 	
+	/**
+	 * fetches all employers
+	 * @return returns employer list
+	 */
 	public List<Employer> getAllEmployers() {
 		List<Employer> empList = new ArrayList<Employer>();
 		employerRepository.findAll().forEach(empList::add);
 		return empList;
 	}
 	
+	/**
+	 * fetches a particular employer
+	 * @param id employer ID
+	 * @return returns an employer object
+	 */
 	public Employer getEmployer(long id) {
 		return employerRepository.findOne(id);
 	}
 
+	/**
+	 * adds an employer
+	 * @param emp employer object
+	 */
 	public void addEmployer(Employer emp) {
 		employerRepository.save(emp);
 	}
 
+	/**
+	 * updates employer
+	 * @param emp employer object
+	 * @param id employer ID
+	 */
 	public void updateEmployer(Employer emp, long id) {
 		employerRepository.save(emp);
 	}
 
+	/**
+	 * deletes employer
+	 * @param id employer ID 
+	 */
 	public void deleteEmployer(long id) {
 		employerRepository.delete(id);
 	}
