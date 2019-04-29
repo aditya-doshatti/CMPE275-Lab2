5a50cd0c3fe9f9653b81580d33bb643245493cee
Aditi Kumari <aditi12395@gmail.com>
2019-04-29 05:06:16 -0700

    Frontend setup and email verification added

---
+++ FinalProject/frontend/src/component/NotAuthorized.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class NotAuthorized extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return ( 
+        <div className="container">
+            <div className="ml-5 mt-5 mr-5 bg-light">
+                <h3 className="ml-3 mt-3 p-4">Verify the link sent to your email to proceed!</h3>
+                <p className="text-danger"></p><a className="ml-3 mt-5 p-4" href="/login">Click her to proceed to Login</a><br></br>
+            </div>
+
+        </div> );
+    }
+}
+ 
+export default  NotAuthorized;
